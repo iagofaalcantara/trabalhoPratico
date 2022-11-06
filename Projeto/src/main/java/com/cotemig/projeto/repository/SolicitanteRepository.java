2bbf7b106548b9e8e09680ec60f1cc4f9ecb1a06
Lívia Fernanda <livia.fsb@gmail.com>
2022-11-06 18:40:17 -0300

    Criação repository solicitante.

--- Projeto/src/main/java/com/cotemig/projeto/repository/SolicitanteRepository.java
+++ Projeto/src/main/java/com/cotemig/projeto/repository/SolicitanteRepository.java
@@ -1,6 +1,5 @@
 package com.cotemig.projeto.repository;
 
-import com.cotemig.projeto.model.Atendente;
 import com.cotemig.projeto.model.Solicitante;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
